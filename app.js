c0d91665c7451a7b9bfa34655a4387607910f05a
Jane-Huve <150970735+Jane-Huve@users.noreply.github.com>
2023-12-18 09:27:53 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express =  require('express')
+const bodyParser= require (body-parser)
+const patient =require(patients)
+
+
+const app = express();
+const PORT =3000;
+
+app.use(bodyParser.json());
+
+const patients =[];
+
+app.post
+
+{
+    "surname"= "Opong"
+    "otherNames"="John"
+    "gender"="male"
+    "phoneNumber"="0965478976"
+    ""
